49db8dcf566463b7393740caf691f3f3343109a1
Sam Hinz <samhinz@live.com>
2025-05-27 12:36:31 +1000

    redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -53,7 +52,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ksuseelectrical">
         <AppContent />
       </BrowserRouter>
     </TooltipProvider>
